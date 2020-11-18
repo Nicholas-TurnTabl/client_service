59c3dc70c87becc1fab018c7c6d7884404336194
Nicholas-TurnTabl <nicholas.dickson@turntabl.io>
2020-11-18 08:30:35 +0000

    first commit

---
+++ src/main/java/com/nyansapo/client_service/ClientServiceApplication.java
@@ -0,0 +1,15 @@
+package com.nyansapo.client_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+public class ClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientServiceApplication.class, args);
+	}
+
+}
